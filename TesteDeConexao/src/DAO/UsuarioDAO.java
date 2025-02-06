4fcd9a5322697c5a19d885ac5092769a514edad8
Matheus Henrique <henriquedemelo08@gmail.com>
2025-02-06 15:09:08 -0300

    Métodos de CRUD criados no DAO

--- TesteDeConexao/src/DAO/UsuarioDAO.java
+++ TesteDeConexao/src/DAO/UsuarioDAO.java
@@ -2,6 +2,10 @@ package DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.ResultSet;
 
 import fabricaDeConexao.ConexaoBD;
 import model.Usuario;
@@ -9,12 +13,98 @@ import model.Usuario;
 public class UsuarioDAO {
     
     public void createUsuario(Usuario usuario) {
+        Connection connection = new ConexaoBD().getConnection();
+        PreparedStatement stmt = null;
+            
+        String insert = "INSERT INTO usuario(nome, email, senha) VALUES (?, ?, ?)";
+        try {
+            stmt = connection.prepareStatement(insert);
+            stmt.setString(1, usuario.getNome());
+            stmt.setString(2, usuario.getEmail());
+            stmt.setString(3, usuario.getSenha());
+            
+            stmt.executeUpdate();
+            System.out.println("Usuario criado!");
+        } catch (SQLException ex) {
+            System.err.println("Erro ao inserir usuario no banco: " + ex.getMessage());
+        } finally {
+             ConexaoBD.closeConnection(connection, stmt);
+        }
+        
+    }
+    
+    public void updateUsuario(Usuario usuario) {
+
+        Connection connection = new ConexaoBD().getConnection();
+        PreparedStatement stmt = null;
+
+        String update = "UPDATE usuario SET nome = ?, email = ?, senha = ? WHERE id = ?";
+            
+        try {    
+            stmt = connection.prepareStatement(update);
+            stmt.setString(1, usuario.getNome());
+            stmt.setString(2, usuario.getEmail());
+            stmt.setString(3, usuario.getSenha());
+            stmt.setInt(4, usuario.getId());
+            
+            stmt.executeUpdate();
+            System.out.println("Usuario atualizado!");
+        } catch (SQLException ex) {
+            System.err.println("Erro ao atualizar usuario: " + ex.getMessage());
+        } finally {
+            ConexaoBD.closeConnection(connection, stmt);
+        }
+    }
+    
+    public void deleteUsuario(Usuario usuario) {
         
         Connection connection = new ConexaoBD().getConnection();
         PreparedStatement stmt = null;
-        
-        String insert = "INSERT INTO usuario(nome, email, senha) VALUES (?, ?, ?)";
-        
+
+        String delete = "DELETE FROM usuario WHERE id = ?";
+        try {    
+            stmt = connection.prepareStatement(delete);
+            stmt.setInt(1, usuario.getId());
+            
+            stmt.executeUpdate();
+            System.out.println("Usuario deletado!");
+        } catch (SQLException ex) {
+            System.err.println("Erro ao deletar usuario: " + ex.getMessage());
+        } finally {
+            ConexaoBD.closeConnection(connection, stmt);
+        }
     }
+    
+    public int selectIdUsuario(Usuario usuario) {
+        
+        Connection connection = new ConexaoBD().getConnection();
+        PreparedStatement stmt = null;
+        ResultSet result = null;
+        
+        int id=0;
+
+        String select = "SELECT id FROM usuario WHERE nome = ? AND email = ? AND senha = ?";
+        
+        try {
+            stmt = connection.prepareStatement(select);
+            result = stmt.executeQuery();
+            
+            result.next();
+            
+            id = result.getInt("usuario.id");
+            
+        } catch (SQLException ex) {
+            System.out.println("Erro ao procurar o usuario: " + ex.getMessage());
+        } finally {
+            ConexaoBD.closeConnection(connection, stmt, result);
+        }
+        
+        return id;
+    }
+    
+    /*public static void main(String[] args) { PARA TESTES 
+        createUsuario(new Usuario("Matheus", "email@email.com", "senha"));
+    } */
+            
     
 }
--- TesteDeConexao/src/fabricaDeConexao/ConexaoBD.java
+++ TesteDeConexao/src/fabricaDeConexao/ConexaoBD.java
@@ -9,7 +9,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ConexaoBD {
-    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/bd_detection";
     private static final String USUARIO = "root";
     private static final String SENHA = "root";
@@ -20,6 +20,17 @@ public class ConexaoBD {
             return DriverManager.getConnection(URL, USUARIO, SENHA);
         } catch (ClassNotFoundException | SQLException ex) {
             throw new RuntimeException("Erro de conexão:" + ex);
+        }
+    }
+    
+    public static void closeConnection(Connection connection, PreparedStatement stmt) {
+        if(connection != null && stmt != null) {
+            try {
+                connection.close();
+                stmt.close();
+            } catch (SQLException ex) {
+                System.out.println("Erro ao fechar conexão: " + ex.getMessage());
+            }
         }
     }
     
