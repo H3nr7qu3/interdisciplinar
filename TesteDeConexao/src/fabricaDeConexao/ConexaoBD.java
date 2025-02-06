d72afb7f1651246a81edbce5c5eb846cbb3d5169
0074950 <0074950@OBD-LAB1-009.ifmg.edu.br>
2025-02-06 08:38:46 -0300

    Criei a classe

---
+++ TesteDeConexao/src/fabricaDeConexao/ConexaoBD.java
@@ -0,0 +1,10 @@
+package fabricaDeConexao;
+
+public class ConexaoBD {
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/bd_detection";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "root";
+    
+    
+}
